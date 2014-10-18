339938d7809437fe46153ce106d75c101033059e
Chris Holdgraf <choldgraf@berkeley.edu>
2014-10-18 16:24:06 -0700

    Updated the plos_search function to be a bit more concise/clear, and to perform more complicated searches

--- doi_retrieval_script.py
+++ doi_retrieval_script.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import json
-from search_and_DOI_utilities import plos_search, plos_dois
+from search_and_DOI_utilities import search_plos, plos_dois
 
 SEARCH_JOURNAL = "PLOS ONE"
-MAX_PAPERS = 20000
-search_results = plos_search(SEARCH_JOURNAL, query_type = "journal", rows = MAX_PAPERS)
+MAX_PAPERS = 200000
+extra_parameters = {'journal': 'PLOS ONE'}
+
+search_results = search_plos('*', extra_parameters=extra_parameters, rows=MAX_PAPERS)
 print "Retrieving " + str(len(search_results)) + " DOIs from PLOS ONE..."
 dois = plos_dois(search_results)
 # jdois = json.dumps(dois)
